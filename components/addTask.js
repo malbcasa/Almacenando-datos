5b660fb34cf17f3a27dad396c5e751ec391c4d1a
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-18 11:12:01 -0300

    Aula 03 completa

--- components/addTask.js
+++ components/addTask.js
@@ -2,37 +2,44 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) => {
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-   // list.appendChild(task);
-  }
-  
- 
-  
-  const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
-    console.log(dateFormat);
-    const list = document.querySelector('[data-list]');
-    const task = document.createElement('li');
-    task.classList.add('card');
+
+    
+  
     input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
-    console.log(value, dateFormat);
+    calendar.value = '';  
     const taskObj = {
       value,
       dateFormat
     }
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push(taskObj);
+    localStorage.setItem('tasks',JSON.stringify(taskList));
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+  }
   
-    taskList.push(taskObj)
-    localStorage.setItem("task",JSON.stringify(taskList));
+ 
   
+  const createTask = ({value, dateFormat}) => {
+    
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    
+    
+    const taskContent = document.createElement('div');
+    console.log(value, dateFormat);
+      
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
