ecb573678d42c8959ca87e7df3d54532695e014e
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-20 00:52:27 -0300

    Aula 04 completa

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,11 +1,26 @@
 import { createTask } from "./addTask.js";
+import dateElement from "./dateElement.js";
+import { uniqueDates } from "./services/date.js";
 
-export const readTasks = () => {
-    const list = document.querySelector(['data-list']);
+export const displayTasks = () => {
+    const list = document.querySelector('[data-list]');
 
     const tasksList  = JSON.parse(localStorage.getItem('tasks')) || [];
+    const dates = uniqueDates(tasksList);
     
-    tasksList.forEach(task => {
-        list.appendChild(task);    
-    });
-}
+    dates.forEach( date =>{
+        
+        const dateMoment = moment(date,'DD/MM/YYYY');
+        list.appendChild(dateElement(date)); 
+        tasksList.forEach((task) => {
+            const taskDate = moment(task.dateFormat,'DD/MM/YYYY');
+            const diff = dateMoment.diff(taskDate);
+            if(diff == 0){
+                list.appendChild(createTask(task));    
+            }
+        });
+    })
+
+    
+};
+
